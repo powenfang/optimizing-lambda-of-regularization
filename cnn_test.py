82e65021e5c98f9b605ef53d9f5b040c4cd1da34
Evan <cylin@nyu.edu>
2021-05-04 17:38:45 -0400

    correct cnn

--- cnn_test.py
+++ cnn_test.py
@@ -49,44 +49,44 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-# class ConvNet(nn.Module):
-#     def __init__(self):
-#         super(ConvNet, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 300, 3)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(300, 300, 3)
-#         self.fc1 = nn.Linear(300 * 6 * 6, 500)
-#         self.fc2 = nn.Linear(500, 10)
-
-#     def forward(self, x):
-#         # -> n, 3, 32, 32
-#         x = self.pool(F.relu(self.conv1(x)))  # -> n, 300, 15, 15
-#         x = self.pool(F.relu(self.conv2(x)))  # -> n, 300, 6, 6
-#         x = x.view(-1, 300 * 6 * 6)            # -> n, 300*6*6
-#         x = F.relu(self.fc1(x))               # -> n, 500
-#         x = self.fc2(x)                       # -> n, 10
-#         return x
-
-# example structure of CNN
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 300, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(300, 300, 3)
+        self.fc1 = nn.Linear(300 * 6 * 6, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
         # -> n, 3, 32, 32
-        x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
-        x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
-        x = x.view(-1, 16 * 5 * 5)            # -> n, 400
-        x = F.relu(self.fc1(x))               # -> n, 120
-        x = F.relu(self.fc2(x))               # -> n, 84
-        x = self.fc3(x)                       # -> n, 10
+        x = self.pool(F.relu(self.conv1(x)))  # -> n, 300, 15, 15
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 300, 6, 6
+        x = x.view(-1, 300 * 6 * 6)            # -> n, 300*6*6
+        x = F.relu(self.fc1(x))               # -> n, 500
+        x = self.fc2(x)                       # -> n, 10
         return x
+
+# example structure of CNN
+# class ConvNet(nn.Module):
+#     def __init__(self):
+#         super(ConvNet, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, 10)
+
+#     def forward(self, x):
+#         # -> n, 3, 32, 32
+#         x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
+#         x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
+#         x = x.view(-1, 16 * 5 * 5)            # -> n, 400
+#         x = F.relu(self.fc1(x))               # -> n, 120
+#         x = F.relu(self.fc2(x))               # -> n, 84
+#         x = self.fc3(x)                       # -> n, 10
+#         return x
 
 
 model = ConvNet().to(device)
